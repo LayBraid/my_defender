4e2f5b2aa301f05a6302130a0b581eba8505cb40
LayBraid <clement.loeuillet@epitech.eu>
2022-02-10 16:46:34 +0100

    change name of parameter for characters

--- src/setup/buttons/hovered_button.c
+++ src/setup/buttons/hovered_button.c
@@ -17,12 +17,12 @@
  * Ça retourne un objet 'hovered_button' setup auto
 */
 
-hovered_button *setup_button(dfd *defender, const float info[12],
+hovered_button *setup_button(dfd *df, const float info[12],
 void (* function)(my_defender_pointer *defender))
 {
     hovered_button *button = malloc(sizeof(hovered_button));
 
-    button->button = setup_a_anim_img(defender, info);
+    button->button = setup_a_anim_img(df, info);
     button->x_click = info[7];
     button->y_click = info[8];
     button->x_max = info[9];
