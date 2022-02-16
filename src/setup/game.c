f0b326d074582985e9101cfa34fae0cfc60d5631
LayBraid <clement.loeuillet@epitech.eu>
2022-02-16 10:28:58 +0100

    set drag and drop at first initialization

--- src/setup/game.c
+++ src/setup/game.c
@@ -20,15 +20,15 @@
 
 void setup_game(my_defender_t *defender)
 {
+    defender->drag = malloc(sizeof(d_d_t));
+    defender->drag->state = NOTHING;
+    defender->drag->id_building = -1;
     defender->window = setup_window(defender);
     defender->list_img = malloc(sizeof(node_img));
     defender->list_buttons = malloc(sizeof(node_button));
     defender->texture = sfTexture_createFromFile("resources/global.png", NULL);
     defender->coins = 0;
     defender->emeralds = 0;
-    defender->drag = malloc(sizeof(d_d_t));
-    defender->drag->state = NOTHING;
-    defender->drag->id_building = -1;
     defender->list_img = setup_img(defender);
     defender->list_buttons = setup_buttons(defender);
     defender->step = OPEN;
