c795ea60313c7cb745ce9c1571225cb322a2bb8b
LayBraid <clement.loeuillet@epitech.eu>
2022-02-18 11:46:41 +0100

    add calcul of correction for center image on cursor

--- src/game/drag_and_drop.c
+++ src/game/drag_and_drop.c
@@ -1,41 +1,40 @@
 /*
 ** EPITECH PROJECT, 2022
-** my_defender
+** my_df
 ** File description:
 ** No file there , just an epitech header example
 */
 
-#include <printf.h>
 #include "struct.h"
 #include "game.h"
 #include "utils.h"
 
-//TODO Bouton 9 == Image 12
-
-void check_on_button(my_defender_t *defender,
+void check_on_button(dfd *df,
 node_button *button, sfVector2i mouse)
 {
-    if (display(button->button->step_display, defender->step) &&
+    if (display(button->button->step_display, df->step) &&
         ((float) mouse.x > button->button->x_click &&
          (float) mouse.x < button->button->x_click + button->button->x_max &&
          (float) mouse.y > button->button->y_click &&
          (float) mouse.y < button->button->y_click + button->button->y_max)) {
-        defender->drag->id_building = button->id + 3;
+        df->drag->id_building = button->id + 3;
+        df->drag->x_correction = button->button->x_click - (float) mouse.x;
+        df->drag->y_correction = button->button->y_click - (float) mouse.y;
         return;
     }
 }
 
-void drag_and_drop(my_defender_t *defender)
+void drag_and_drop(dfd *df)
 {
-    sfVector2i mu = sfMouse_getPositionRenderWindow(defender->window->window);
-    node_button *tmp = defender->list_buttons;
+    sfVector2i mu = sfMouse_getPositionRenderWindow(df->window->window);
+    node_button *tmp = df->list_buttons;
 
-    if (defender->drag->state != NOTHING)
+    if (df->drag->state != NOTHING)
         return;
     while (tmp->id < tmp->next->id) {
-        check_on_button(defender, tmp, mu);
+        check_on_button(df, tmp, mu);
         tmp = tmp->next;
     }
-    check_on_button(defender, tmp, mu);
-    defender->drag->state = BUILDING;
+    check_on_button(df, tmp, mu);
+    df->drag->state = BUILDING;
 }
