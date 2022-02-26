216304d574cc86f77fe21274bc117609a2190a27
LayBraid <clement.loeuillet@epitech.eu>
2022-02-26 17:33:17 +0100

    comment error

--- src/game/roads/find_exit.c
+++ src/game/roads/find_exit.c
@@ -16,10 +16,12 @@ void exit_finder(int ** map)
                 index++;
             }
             if (map[index][i -1] != 0 && map[index][i +1] != 0 && map[index+ 1 ][i] != 0 && map[index - 1 ][i -1] != 0 && map[index][i +1]!= 0) {
-                return 64;
+                //return 64;
+                return;
             }
             if (map[index][i] == 4) {
-                return 0;
+                //return 0;
+                return;
             }
     }
 }
