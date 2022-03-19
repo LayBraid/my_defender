a323eae61c7e8315e0c6ec835702e64c48366302
LayBraid <clement.loeuillet@epitech.eu>
2022-03-19 23:11:13 +0100

    :white_check_mark: update hits

--- src/game/enemies/hit.c
+++ src/game/enemies/hit.c
@@ -11,6 +11,9 @@
 void hit_enemy(dfd *df, node_enemy *enemy, node_earthly_build *earthly)
 {
     enemy->enemy->life -= earthly->build->hit;
+    if (enemy->enemy->life < 0)
+        enemy->enemy->life = 0;
     add_hit_enemy(df, enemy, simple_id(1, enemy->id));
     add_hit_enemy(df, enemy, simple_id(0, enemy->id));
+    df->coins += 3 + (df->wave * 2);
 }
