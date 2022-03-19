bb60a21b3f225f6a61ae765d8b119b5a62f11a2a
LayBraid <clement.loeuillet@epitech.eu>
2022-03-19 21:53:42 +0100

    :white_check_mark: update price of buildings

--- include/buildings.h
+++ include/buildings.h
@@ -14,37 +14,37 @@
     #define PRICE_INIT_TOWER_1 5
 
     #define LIFE_INIT_TOWER_2 25
-    #define PRICE_INIT_TOWER_2 5
+    #define PRICE_INIT_TOWER_2 10
 
     #define LIFE_INIT_TOWER_3 40
-    #define PRICE_INIT_TOWER_3 5
+    #define PRICE_INIT_TOWER_3 15
 
     #define LIFE_INIT_TOWER_4 60
-    #define PRICE_INIT_TOWER_4 5
+    #define PRICE_INIT_TOWER_4 20
 
     #define LIFE_INIT_TANK_1 20
-    #define PRICE_INIT_TANK_1 5
+    #define PRICE_INIT_TANK_1 10
 
     #define LIFE_INIT_TANK_2 35
-    #define PRICE_INIT_TANK_2 5
+    #define PRICE_INIT_TANK_2 20
 
     #define LIFE_INIT_MISSILE_1 20
-    #define PRICE_INIT_MISSILE_1 5
+    #define PRICE_INIT_MISSILE_1 15
 
     #define LIFE_INIT_MISSILE_2 35
-    #define PRICE_INIT_MISSILE_2 5
+    #define PRICE_INIT_MISSILE_2 20
 
     #define LIFE_INIT_MISSILE_3 50
-    #define PRICE_INIT_MISSILE_3 5
+    #define PRICE_INIT_MISSILE_3 25
 
     #define LIFE_INIT_MISSILE_4 70
-    #define PRICE_INIT_MISSILE_4 5
+    #define PRICE_INIT_MISSILE_4 30
 
     #define LIFE_INIT_PLANE_1 35
-    #define PRICE_INIT_PLANE_1 5
+    #define PRICE_INIT_PLANE_1 20
 
     #define LIFE_INIT_PLANE_2 50
-    #define PRICE_INIT_PLANE_2 5
+    #define PRICE_INIT_PLANE_2 30
 
 void add_to_list_build(node_earthly_build **node, build_t *build);
 
