8ee0aaf8c97f5d577f22f9fc5081f3c2c7e3e626
LayBraid <clement.loeuillet@epitech.eu>
2022-03-19 23:00:28 +0100

    :bug: fix chained list for anim of hit

--- src/game/enemies/clocks/hits/add.c
+++ src/game/enemies/clocks/hits/add.c
@@ -12,6 +12,7 @@ void add_hit_enemy(dfd *df, node_enemy *enemy, const int *info)
 {
     node_movement_clock *new = malloc(sizeof(node_movement_clock));
     node_anim_enemy_t *tmp = df->enemies_clocks;
+    node_movement_clock *update;
 
     while(tmp->id != enemy->id)
         tmp = tmp->next;
@@ -19,12 +20,13 @@ void add_hit_enemy(dfd *df, node_enemy *enemy, const int *info)
     new->type = info[0];
     new->id = info[1];
     new->next = NULL;
+    update = tmp->movement;
 
     if (tmp->hit == NULL) {
         tmp->hit = new;
         return;
     }
-    while (tmp->hit->next != NULL)
-        tmp->hit = tmp->hit->next;
-    tmp->hit->next = new;
+    while (update->next != NULL)
+        update = update->next;
+    update->next = new;
 }
