6b0422bc6ba1800dc9d470ce6dbd615e70e2bdb8
elie chardin <elie.chardin@epitech.eu>
2022-03-20 19:54:49 +0100

    click2

--- src/game/actions/buttons_3.c
+++ src/game/actions/buttons_3.c
@@ -35,5 +35,5 @@ void click_1(my_defender_pointer *df)
 void click_2(my_defender_pointer *df)
 {
     dfd *d = (dfd *) df;
-    d->click = sfMusic_createFromFile("resources/click.ogg");
+    d->click = sfMusic_createFromFile("resources/click2.wav");
 }
